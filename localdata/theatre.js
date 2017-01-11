9363b2831f1890c04f86dbf7873b2404d2cf58e4
Cedric-song <kengmun@126.com>
2017-01-12 00:46:42 +0800

    add some pages

---
+++ localdata/theatre.js
@@ -0,0 +1,12 @@
+{
+    [
+            {
+                 theatreName:"橙天嘉禾影城(上地店)",
+                 theatreAddress:"海淀区上地南口华联商厦4F",
+                 leastPrice:"24.4",
+                 blueTags:["座","退","改签","60帧厅","4K厅"],
+                 orangeTags:["小吃","折扣卡"],
+                 promotion:["惠","太空旅客特惠"]   
+            }
+    ]
+}
--- pages/movie/movie.js
+++ pages/movie/movie.js
@@ -1,12 +1,19 @@
 Page({
   data: {
-    array: [{
-      mode: 'scaleToFill',
-      text: 'scaleToFill：不保持纵横比缩放图片，使图片完全适应'
-    }],
-    src: '../../resources/cat.jpg'
-  },
-  imageError: function(e) {
-    console.log('image3发生error事件，携带值为', e.detail.errMsg)
+    cityName: "北京",
+    searchContent: "找影视剧、影院",
+    list: [
+      {
+        imageUrl: "../../localdata/imgs/stars.jpg",
+        movieName: "星球大战外传：侠盗一号",
+        blueTags: ["3D", "MAX"],
+        orangeTags: ["点映"],
+        movieScore: "7.8",
+        movieType: "动作,冒险,科幻",
+        movieActors: "主演:菲丽希缇,迭戈鲁纳,艾伦艾弗森,蒂姆邓肯",
+        movieInfo: "今天173家影院放映2233场",
+        movieStatus: "购票"
+      }
+    ]
   }
 })
